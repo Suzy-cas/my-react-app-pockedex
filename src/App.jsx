0bfe192a305d014604d55f39dbaf523dae194864
CASSAR SUZY <contact@scassar.com>
2023-10-10 11:27:51 +0200

    created PokemonCard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import PokemonCard from "./components/PokemonCard"
 
 
 function App() {
-  
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
       <div>
-        <PokemonCard />
-      </div>
-      
-  
+         <PokemonCard />
+     </div>
+    </>
+  )
 }
 
 export default App;
