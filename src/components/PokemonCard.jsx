26e11bf6221a1e0036261216e0155370a6756b52
CASSAR SUZY <contact@scassar.com>
2023-10-10 11:18:00 +0200

    created new component PokemonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,7 @@
+function PokemonCard() {
+    <><figure>
+        <img scr="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
+    </figure><figcaption>Bullbasaur</figcaption></>
+
+}
+export default PokemonCard;
